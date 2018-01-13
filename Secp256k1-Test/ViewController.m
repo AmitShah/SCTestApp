161859d057177ae990b95b2655d1406666f9cc4b
AmitYYZ <amitshah@Amits-MacBook-Pro.local>
2018-01-12 20:29:59 -0500

    change the hex string to data parsing to use the Transaction.h dataFromChar function as it accounts for odd length hex sequence of bytes by prepending 0 as needed

--- Secp256k1-Test/ViewController.m
+++ Secp256k1-Test/ViewController.m
@@ -29,22 +29,22 @@
 }
 
 - (NSData *)dataFromHexString {
-    
-    const char *chars = [self UTF8String];
-    int i = 0, len = self.length;
-    
-    NSMutableData *data = [NSMutableData dataWithCapacity:len / 2];
-    char byteChars[3] = {'\0','\0','\0'};
-    unsigned long wholeByte;
-    
-    while (i < len) {
-        byteChars[0] = chars[i++];
-        byteChars[1] = chars[i++];
-        wholeByte = strtoul(byteChars, NULL, 16);
-        [data appendBytes:&wholeByte length:1];
-    }
-    //unsigned char *bytePtr = (unsigned char *)[data bytes];
-    return data;
+    return dataFromChar([self UTF8String],(int)[self length]  );
+//    const char *chars = [self UTF8String];
+//    int i = 0, len = self.length;
+//    
+//    NSMutableData *data = [NSMutableData dataWithCapacity:len / 2];
+//    char byteChars[3] = {'\0','\0','\0'};
+//    unsigned long wholeByte;
+//    
+//    while (i < len) {
+//        byteChars[0] = chars[i++];
+//        byteChars[1] = chars[i++];
+//        wholeByte = strtoul(byteChars, NULL, 16);
+//        [data appendBytes:&wholeByte length:1];
+//    }
+//    //unsigned char *bytePtr = (unsigned char *)[data bytes];
+//    return data;
 }
 
 
@@ -74,22 +74,22 @@ static int custom_nonce_function_rfc6979(unsigned char *nonce32, const unsigned 
     Transaction* t = [[Transaction alloc] init];
     mp_int gasLimit;
     mp_init(&gasLimit);
-    mp_set(&gasLimit, 100000);
+    mp_set(&gasLimit, 210000);
     t.gasLimit = gasLimit;
     
     mp_int gasPrice;
     mp_init(&gasPrice);
-    mp_set(&gasPrice, 4700000);
+    mp_set(&gasPrice, 4700);
     t.gasPrice = gasPrice;
     
     mp_int nonce;
     mp_init(&nonce);
-    mp_set(&nonce, 2);
+    mp_set(&nonce, 0);
     t.nonce = nonce;
     
     mp_int value;
     mp_init(&value);
-    mp_set(&value, 120);
+    mp_set(&value, 1200);
     t.value = value;
     
     
